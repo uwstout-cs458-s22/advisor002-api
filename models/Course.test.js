b2a7e563aea42e05f4bcaa778361d25831d7fdf5
Matthew Friemoth <61516300+MattFriemoth@users.noreply.github.com>
2022-03-07 17:17:07 -0600

    Create Course.test.js

---
+++ models/Course.test.js
@@ -0,0 +1,25 @@
+const log = require('loglevel');
+const { db } = require('../services/database');
+const env = require('../services/environment');
+const User = require('./Course');
+
+beforeAll(() => {
+  log.disableAll();
+});
+
+
+jest.mock('../services/database.js', () => {
+  return {
+    db: {
+      query: jest.fn(),
+    },
+  };
+});
+
+jest.mock('../services/environment.js', () => {
+  return {
+    masterAdminEmail: 'master@gmail.com',
+  };
+});
+
+
