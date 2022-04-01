c9042c146014310d613f1c5594cf3e76a3e498d5
Jake Ellingson <ellingsonj1535@my.uwstout.edu>
2022-04-01 18:29:26 -0500

    Ensuring year is in semester table

--- services/initDatabase.sql
+++ services/initDatabase.sql
@@ -69,6 +69,7 @@ CREATE TABLE IF NOT EXISTS "courseCategory" (
 -- Changing fields
 ALTER TABLE "course" DROP COLUMN IF EXISTS "courseId";
 ALTER TABLE "course" ADD COLUMN IF NOT EXISTS "section" text;
+ALTER TABLE "semester" ADD COLUMN IF NOT EXISTS "year" integer;
 
 -- Run to quickly remove all tables for testing.
 -- DROP TABLE IF EXISTS "semester" cascade;
